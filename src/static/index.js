454febffcac5754a270aeecdd8300356d50b4eaa
Marta Jablonska <marta.jablonska@vmlyr.com>
2021-12-07 11:34:31 +0100

    add scoring points

--- src/static/index.js
+++ src/static/index.js
@@ -1,9 +1,39 @@
 const allSquares = [...document.querySelectorAll(".square")];
-let randomSquare = Math.floor(Math.random()*25+0);
+let randomSquareNumb = Math.floor(Math.random()*25+0);
+let randomSquare;
+let randomSquareId;
+let currentClick;
+let clickedSquareId;
+let gameResult = 0;
+let livesNum = 3;
+
 
 function showRandomSquare() {
-    allSquares[randomSquare].classList.add('square__clicked');
+    allSquares[randomSquareNumb].classList.add('square__clicked');
+    randomSquare = allSquares[randomSquareNumb];
+    randomSquareId = randomSquare.id;
 };
 
 showRandomSquare();
+
+window.addEventListener('click', function(e) {
+    currentClick = e.target;
+    clickedSquareId = currentClick.id;
+
+    if(randomSquareId===clickedSquareId) {
+        console.log("bingo!");
+        gameResult +=1;
+    } else {
+        alert("Straciłeś życie gapo!");
+        livesNum -=1;
+    } 
+
+    if(livesNum <= 0) {
+        this.alert("Koniec gry");
+    }
+
+    console.log(gameResult);
+    console.log(livesNum);
+});
+
 
